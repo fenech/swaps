bb1b4f141d09c6208270cbfbf5a63d7e46d63817
Tom Fenech <reggaebristol@gmail.com>
2014-09-01 18:18:48 +0100

    allow multiple copies of each sticker

--- got.pl
+++ got.pl
@@ -11,24 +11,19 @@ die "can only add or remove" if ($opt_a && $opt_r);
 my $file = shift;
 
 sub readlist;
-my %got;
-map { ++$got{$_} } readlist $file;
+my %stickers;
+map { ++$stickers{$_} } readlist $file;
 
 my ($added, $removed, $failed) = (0, 0, 0);
 
 while (my $s = shift) {
     if ($opt_a) {
-	unless (exists $got{$s}) {
-	    $got{$s} = 1;
-	    ++$added;
-	} else {
-	    print "already got sticker $s\n";
-	    ++$failed;
-	}
+	++$stickers{$s};
+	++$added;
     }
     elsif ($opt_r) {
-	if (exists $got{$s}) {
-	    delete $got{$s};
+	if (exists $stickers{$s} && $stickers{$s} > 0) {
+	    delete $stickers{$s} if --$stickers{$s} == 0;	    
 	    ++$removed;
 	} else {
 	    print "sticker $s not found\n";
@@ -38,10 +33,10 @@ while (my $s = shift) {
 }
 
 print "$added stickers added, $removed stickers removed, $failed failed\n";
-print scalar keys %got, " stickers in $file\n";
+print scalar keys %stickers, " stickers in $file\n";
 
 open my $out, '>', $file;
-print $out "$_\n" for join("\n", map { ("$_") x $got{$_} } sort {$a <=> $b} keys %got);
+print $out "$_\n" for join("\n", map { ("$_") x $stickers{$_} } sort {$a <=> $b} keys %stickers);
 
 sub readlist {
     local $/;
